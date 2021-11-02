96a5a3d2eae4e0b277acbe2280147ccba3d5cc23
Rashid-Hussain-Cse <rashidhussain.cse@gmail.com>
2021-11-02 15:47:06 +0530

    v2

--- shop/views.py
+++ shop/views.py
@@ -13,17 +13,17 @@ def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    return render(request,'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request,'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at Search")
+    return render(request,'shop/search.html')
 
 def productView(request):
-    return HttpResponse("We are at productView")
+    return render(request,'shop/productView.html')
 
 def checkout(request):
-    return HttpResponse("We are at request")
+    return render(request,'shop/checkout.html')
  
